9864d1483798647784e2b7cac3bdbd016e93e103
Ling Peng <LP806562@163.com>
2023-09-08 18:03:23 +0800

    first commit

---
+++ app/src/main/java/com/example/myapplication/Str.java
@@ -0,0 +1,6 @@
+package com.example.myapplication;
+
+public class Str {
+    public static final int REQUEST_CODE = 123; // 任何唯一的整数值
+    public static final String COMMENT_SUCCESS = "点击评论成功"; // 任何唯一的整数值
+}
--- app/src/main/java/com/example/myapplication/constStr.java
+++
@@ -1,5 +0,0 @@
-package com.example.myapplication;
-
-public class constStr {
-    public static final int REQUEST_CODE = 123; // 任何唯一的整数值
-}
